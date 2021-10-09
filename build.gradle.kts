c0e7fc5d857b7da24bbab4d60dcdc2f5a6753405
Patryk Rutkowski <patrykk.rutkowski@gmail.com>
2021-10-09 19:46:35 +0200

    Added shadow plugin for releasing JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,19 @@ val exposedVersion: String by project
 plugins {
     application
     kotlin("jvm") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "pl.psplus"
 version = "0.0.1"
+
 application {
     mainClass.set("pl.psplus.ApplicationKt")
+    mainClassName = "pl.psplus.ApplicationKt"
 }
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
@@ -31,4 +35,12 @@ dependencies {
     implementation("com.zaxxer:HikariCP:5.0.0")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+}
+
+tasks{
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", "pl.psplus.ApplicationKt"))
+        }
+    }
 }
